f19a384a29199677db62097688bdf76c34341d23
Rafael P. Josephino <87491488+RafaelPJosephino@users.noreply.github.com>
2025-08-29 16:39:41 -0300

    Set AppShell as startup page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,10 @@
-﻿namespace FilaSinuca;
+namespace FilaSinuca;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-	}
-
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
-	}
+        public App()
+        {
+                InitializeComponent();
+                MainPage = new AppShell();
+        }
 }
