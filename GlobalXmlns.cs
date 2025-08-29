1fb6e82cc07c9e272e6817e8bcb935a9baf5b92e
rafael.josephino <rafael.puckoski.josephino@gmail.com>
2025-08-29 16:55:06 -0300

    fix

--- GlobalXmlns.cs
+++ GlobalXmlns.cs
@@ -1,2 +1,1 @@
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FilaSinuca")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FilaSinuca.Pages")]
